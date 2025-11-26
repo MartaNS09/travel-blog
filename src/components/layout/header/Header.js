0349edfc6ca1e6ae3b9d794b117378f7db79b6ca
MartaNS09 <martacrystal9@gmail.com>
2025-11-26 12:19:45 +0300

    хедер изменения заголовка для других страниц

--- src/components/layout/header/Header.js
+++ src/components/layout/header/Header.js
@@ -1,41 +1,12 @@
-// import React from "react";
-// import { Link } from "react-router-dom";
-// import Logo from "../../ui/Logo";
-// import "./Header.scss";
-
-// const Header = () => {
-//   return (
-//     <header className="header">
-//       <div className="header__hero">
-//         <div className="header__content">
-//           <div className="header__nav">
-//             <Link to="/">
-//               <Logo />
-//             </Link>
-//             <div className="header__actions">
-//               <button className="btn--login">Войти</button>
-//             </div>
-//           </div>
-
-//           <div className="header__divider"></div>
-
-//           <div className="header__title">
-//             <h1>ТАМ, ГДЕ МИР НАЧИНАЕТСЯ С ПУТЕШЕСТВИЙ</h1>
-//           </div>
-//         </div>
-//       </div>
-//     </header>
-//   );
-// };
-
-// export default Header;
-
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import Logo from "../../ui/Logo";
 import "./Header.scss";
 
 const Header = () => {
+  const location = useLocation();
+  const isHomePage = location.pathname === "/";
+
   return (
     <header className="header">
       <div className="header__hero">
@@ -54,7 +25,11 @@ const Header = () => {
           <div className="header__divider"></div>
 
           <div className="header__title">
-            <h1>ТАМ, ГДЕ МИР НАЧИНАЕТСЯ С ПУТЕШЕСТВИЙ</h1>
+            <h1>
+              {isHomePage
+                ? "ТАМ, ГДЕ МИР НАЧИНАЕТСЯ С ПУТЕШЕСТВИЙ"
+                : "ИСТОРИИ ВАШИХ ПУТЕШЕСТВИЙ"}
+            </h1>
           </div>
         </div>
       </div>
